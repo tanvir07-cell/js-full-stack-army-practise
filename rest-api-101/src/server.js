0b3422a4387b2b7c23d47a8f46cf55ba2fcc194a
tanvir07-cell <tanvir15-14402@diu.edu.bd>
2023-01-10 21:56:51 +0700

    for the delete request

--- rest-api-101/src/server.js
+++ rest-api-101/src/server.js
@@ -52,6 +52,46 @@ app.patch("/:id", async (req, res) => {
   res.status(200).json(player);
 });
 
+// PUT request:
+app.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const data = await fs.readFile(dbLocation);
+  const players = JSON.parse(data);
+
+  let player = players.find((p) => p.id === id);
+
+  // jehetu put request tai player nah thake notun kore player create korte hobe:
+  if (!player) {
+    player = {
+      id: shortid.generate(),
+      ...req.body,
+    };
+    players.push(player);
+  } else {
+    player.name = req.body.name;
+    player.country = req.body.country;
+    player.rank = req.body.rank;
+  }
+  await fs.writeFile(dbLocation, JSON.stringify(players));
+  return res.status(200).json(player);
+});
+
+// Delete the player:
+app.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  const data = await fs.readFile(dbLocation);
+  const players = JSON.parse(data);
+  const player = players.find((p) => p.id === id);
+
+  if (!player) {
+    return res.status(400).json({ message: `player ${id} does not exist!` });
+  }
+  // jei id gula match nah hoy oisob id gula niye ekti array korbo taholei jei id ti dibo oi id er data remove hoye jabe:
+  const newPlayers = players.filter((p) => p.id !== id);
+  await fs.writeFile(dbLocation, JSON.stringify(newPlayers));
+  return res.status(203).send();
+});
+
 // create the players and it's save in the src/data.json file automatically:
 app.post("/", async (req, res) => {
   const player = {
