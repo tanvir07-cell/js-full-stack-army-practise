b5dc36869cdb544e24a0bbb8591cbe1bf77440f1
tanvir07-cell <tanvir15-14402@diu.edu.bd>
2023-01-10 15:35:22 +0700

    for the patch req

--- rest-api-101/src/server.js
+++ rest-api-101/src/server.js
@@ -18,6 +18,40 @@ app.get("/health", (_req, res) => {
   res.status(200).json({ message: "OK" });
 });
 
+app.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const data = await fs.readFile(dbLocation);
+  const players = JSON.parse(data);
+  const player = players.find((p) => p.id === id);
+
+  if (!player) {
+    return res.status(404).json({ message: `${id} is not found` });
+  }
+  res.status(200).json(player);
+});
+
+// PATCH -- only used for update the some data if we need to update the whole data then we need to use the PUT method:
+
+app.patch("/:id", async (req, res) => {
+  const id = req.params.id;
+  const data = await fs.readFile(dbLocation);
+  const players = JSON.parse(data);
+  const player = players.find((p) => p.id === id);
+
+  if (!player) {
+    return res.status(404).json({
+      message: `${id} is not found so not updated this player's info`,
+    });
+  }
+  player.name = req.body.name || player.name;
+  player.country = req.body.country || player.country;
+  player.rank = req.body.rank || player.rank;
+
+  //   now update the player's in the database(src/server.js)
+  await fs.writeFile(dbLocation, JSON.stringify(players));
+  res.status(200).json(player);
+});
+
 // create the players and it's save in the src/data.json file automatically:
 app.post("/", async (req, res) => {
   const player = {
@@ -42,18 +76,6 @@ app.get("/", async (req, res) => {
   const data = await fs.readFile(dbLocation);
   const players = JSON.parse(data);
   res.status(200).json(players);
-});
-
-app.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  const data = await fs.readFile(dbLocation);
-  const players = JSON.parse(data);
-  const player = players.find((p) => p.id === id);
-
-  if (!player) {
-    return res.status(404).json({ message: `${id} is not found` });
-  }
-  res.status(200).json(player);
 });
 
 app.listen(port, () => {
